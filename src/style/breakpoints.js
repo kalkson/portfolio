6c88da949a8ba5009b52b185d2ecab061bc19d9a
Damian Kalka <kalka762@gmail.com>
2021-02-10 19:25:29 +0100

    initial for development

---
+++ src/style/breakpoints.js
@@ -0,0 +1,9 @@
+const device = {
+  mobile: `(min-width: 425px)`,
+  tablet: `(min-width: 768px)`,
+  laptopM: `(min-width: 1024px)`,
+  laptopL: `(min-width: 1366px)`,
+  desktop: `(min-width: 1920px)`,
+};
+
+export default device;
